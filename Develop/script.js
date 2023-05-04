31f401e00de05a789458662fb75a5c70b060d913
Isoel Rodriguez <isoelrodriguez@gmail.com>
2023-05-04 17:47:18 -0400

    added password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,24 @@
 // Assignment code here
-
-
+let alphabet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var alphalength=alphabet.length
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword(){
+  console.log(alphalength)
+  
+    let chars = alphabet;
+
+let newPassword = "";
+for (let i = 0; i < 8; i++) {
+
+  newPassword += chars.charAt(Math.floor(Math.random() * alphalength));
+  document.getElementById("password").innerHTML=newPassword
+}
+  
+  
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
